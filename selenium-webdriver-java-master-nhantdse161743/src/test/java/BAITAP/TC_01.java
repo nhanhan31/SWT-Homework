bf08ed5c468762b0decf4adccd62bba76191dc76
nhanhan31 <109142635+nhanhan31@users.noreply.github.com>
2023-10-26 14:48:28 +0700

    Add files via upload

---
+++ selenium-webdriver-java-master-nhantdse161743/src/test/java/BAITAP/TC_01.java
@@ -0,0 +1,65 @@
+/*
+
+Test Steps
+
+Step 1. Go to http://live.techpanda.org/
+
+Step 2. Verify Title of the page
+
+Step 3. Click on -> MOBILE -> menu
+
+Step 4. In the list of all mobile , select SORT BY -> dropdown as name
+
+Step 5. Verify all products are sorted by name
+
+*/
+package BAITAP;
+
+import driver.driverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+@Test
+public class TC_01 {
+    public static void testTC01() {
+        int scc = 0;
+        StringBuffer verificationErrors = new StringBuffer();
+        WebDriver driver = driverFactory.getChromeDriver();
+        try {
+            //Step 1. Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+            //Step 2. Verify Title of the page
+            String demoSite = driver.findElement(By.cssSelector("h2")).getText();
+            System.out.println(demoSite);
+            try{
+                AssertJUnit.assertEquals("THIS IS DEMO SITE FOR   ",demoSite);
+            } catch (Exception e) {
+                verificationErrors.append(e.toString());
+            }
+            Thread.sleep(2000);
+            //Step 3. Click on -> MOBILE -> menu
+            driver.findElement(By.linkText("MOBILE")).click();
+            Thread.sleep(2000);
+            //Step 4. In the list of all mobile , select SORT BY -> dropdown as name
+            new Select(driver.findElement(By.xpath("(//select[@title='Sort By'])[1]"))).selectByVisibleText("Name");
+            Thread.sleep(2000);
+            //Step 5. Verify all products are sorted by name
+            scc = (scc+1);
+            TakesScreenshot screenshot =((TakesScreenshot)driver);
+            File srcFile= screenshot.getScreenshotAs(OutputType.FILE);
+            String png =("D:\\Screenshots/" + scc +".png");
+            FileHandler.copy(srcFile, new File(png));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+}
---
+++ selenium-webdriver-java-master-nhantdse161743/src/test/java/BAITAP/TC_02.java
@@ -0,0 +1,57 @@
+/*
+
+Test Steps:
+
+1. Goto http://live.techpanda.org/
+
+2. Click on -> MOBILE -> menu
+
+3. In the list of all mobile , read the cost of Sony Xperia mobile (which is $100)
+
+4. Click on Sony Xperia mobile
+
+5. Read the Sony Xperia mobile from detail page.
+
+6. Compare Product value in list and details page should be equal ($100).
+
+*/
+package BAITAP;
+
+import driver.driverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class TC_02 {
+    @Test
+    public static void testTC02(){
+        StringBuffer verificationErrors = new StringBuffer();
+        WebDriver driver = driverFactory.getChromeDriver();
+        try {
+            //Step 1. Go to http://live.techpanda.org/
+            driver.get("http://live.techpanda.org/");
+            //Step 2. Click on -> MOBILE -> menu
+            driver.findElement(By.linkText("MOBILE")).click();
+            Thread.sleep(2000);
+            //3. In the list of all mobile , read the cost of Sony Xperia mobile (which is $100)
+            String demoSite1 = driver.findElement(By.xpath("(//span[contains(text(),'$100.00')])[1]")).getText();
+            System.out.println(demoSite1);
+            //4. Click on Sony Xperia mobile
+            driver.findElement(By.linkText("SONY XPERIA")).click();
+            Thread.sleep(2000);
+            //5. Read the Sony Xperia mobile from detail page.
+            String demoSite2 = driver.findElement(By.xpath("(//span[@class='price'])[1]")).getText();
+            System.out.println(demoSite2);
+            //6. Compare Product value in list and details page should be equal ($100).
+            try{
+                AssertJUnit.assertEquals(demoSite1,demoSite2);
+            } catch (Exception e) {
+                verificationErrors.append(e.toString());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+}
